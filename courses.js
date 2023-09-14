906eab925a25f496b04c143fa9040ec125f56733
Mizuki <74822715+MatzdaMizuki@users.noreply.github.com>
2023-09-14 15:45:44 +0400

    Update courses.js

--- courses.js
+++ courses.js
@@ -1,17 +1,17 @@
 function initCourseTemplates() {
   var courseData = {
-      course_start_date: "Mon 28.8",
-      course_end_date: "Tue 15.10",
-      option1_date_1: "29_8",
-      option1_date_display_1: "Tue 29.9 at 12:00-14:00cet",
-      option2_date_1: "31_8",
-      option2_date_display_1: "Thu 31.8 at 19:30-21:30cet",
+      course_start_date: "31.10",
+      course_end_date: "08.12",
+      option1_date_1: "31_10",
+      option1_date_display_1: "Tue 31.10 at 12:00-14:00cet",
+      option2_date_1: "02_11",
+      option2_date_display_1: "Thu 2.11 at 19:30-21:30cet",
 
-      option1_date_2: "19_9",
-      option1_date_display_2: "Tue 19.9 at 12:00-14:00cet",
+      option1_date_2: "28_11",
+      option1_date_display_2: "Tue 28.11 at 12:00-14:00cet",
       option2_date_2: "21_9",
-      option2_date_display_2: "Thu 21.9 at 19:30-21:30cet",
-      //... other course data ...
+      option2_date_display_2: "Thu 31.11 at 19:30-21:30cet",
+      
   };
 
   var courseTemplate = Handlebars.compile(document.getElementById("course-template").innerHTML);
